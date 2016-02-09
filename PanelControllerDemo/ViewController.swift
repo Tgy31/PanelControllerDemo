9a0618de478fd1ff5ed25dd2fe745c873bf592e0
Tanguy Helesbeux <thelesbeux@hevaweb.com>
2016-02-09 17:19:33 +0100

    Initial Commit

---
+++ PanelControllerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PanelControllerDemo
+//
+//  Created by Tanguy Helesbeux on 09/02/2016.
+//  Copyright © 2016 HEVA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
